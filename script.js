f0e13d7cd721da17b99158227fb9ffc1d480ccb7
Ole-Kristian-Haaseth-Rudjord <s354564@oslomet.no>
2021-10-05 18:55:43 +0200

    Responsivness & general optimization

---
+++ script.js
@@ -0,0 +1,11 @@
+// Drop-Down menu in Navbar for smaller screens
+// const dropDown = document.getElementsByClassName("Navbar-Drop-Down");
+// const navbarUl = document.getElementsByClassName("Navbar-ul");
+
+// dropDown.addEventListener('click', () => {
+//     navbarUl.classList.toggle('active')
+// })
+
+// function myFunction() {
+//     document.getElementsByClassName("Navbar-ul").classList.toggle("show");
+// }
