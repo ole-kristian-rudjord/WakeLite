25cfb5c159211c46da40e5724aa4b2ff55313825
Ole-Kristian-Haaseth-Rudjord <s354564@oslomet.no>
2021-10-06 18:51:57 +0200

    Drop down navbar on smaller screens with JS

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // Drop-Down menu in Navbar for smaller screens
-// const dropDown = document.getElementsByClassName("Navbar-Drop-Down");
-// const navbarUl = document.getElementsByClassName("Navbar-ul");
+window.onload = function() {
+    const dropDown = document.getElementById('Navbar-Drop-Down');
+    const navbarUl = document.getElementById('Navbar-ul');
 
-// dropDown.addEventListener('click', () => {
-//     navbarUl.classList.toggle('active')
-// })
-
-// function myFunction() {
-//     document.getElementsByClassName("Navbar-ul").classList.toggle("show");
-// }
+    dropDown.addEventListener('click', () => {
+    navbarUl.classList.toggle('Navbar-active');
+    });
+};
