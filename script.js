0586baf29019a3795cd2833faabf2df3b1d6a61a
Ole-Kristian-Haaseth-Rudjord <s354564@oslomet.no>
2021-10-07 12:53:21 +0200

    fixed sidebar on smaller screens & changed sidebar icon

--- script.js
+++ script.js
@@ -5,7 +5,20 @@ window.onload = function() {
 
     dropDown.addEventListener('click', () => {
         navbarUl.classList.toggle('Navbar-active');
+        setTimeout(() => {
+            navbarUl.classList.toggle('Navbar-active2');
+        }, 0);
     });
+    
+
+    var navbarLinks = document.getElementsByClassName('Navbar-Links');
+
+    for (var i = 0; i < navbarLinks.length; i++) {
+        navbarLinks[i].addEventListener('click', () => {
+            navbarUl.classList.remove('Navbar-active');
+            navbarUl.classList.remove('Navbar-active2');
+        });
+    };
 
     
     const contactSubmit = document.getElementById('Contact-Submit');
