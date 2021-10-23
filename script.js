db94a715ba3d3ae4f5cbee132151fad828ff47b6
Ole-Kristian-Haaseth-Rudjord <s354564@oslomet.no>
2021-10-23 16:02:39 +0200

    Styling and more functionality on digital prototype

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// Drop-Down menu in Navbar for smaller screens
+
 window.onload = function() {
+    console.log("Welcome to the console :) \n-Ole Kristian Rudjord")
+
+// Drop-Down menu in Navbar for smaller screens
     const dropDown = document.getElementById('Navbar-Drop-Down');
     const navbarUl = document.getElementById('Navbar-ul');
 
@@ -10,13 +13,109 @@ window.onload = function() {
         }, 0);
     });
 
+// Order size
+    const ProductSize = document.getElementById('Order-Images-Div');
+
+    const ProductSizeSmall = document.getElementById('Order-Size-Small');
+    const ProductSizeMedium = document.getElementById('Order-Size-Medium');
+    const ProductSizeLarge = document.getElementById('Order-Size-Large');
+
+    const ProductDimensionsSmall = document.getElementById('Order-Images-Dimensions-Small');
+    const ProductDimensionsMedium = document.getElementById('Order-Images-Dimensions-Medium');
+    const ProductDimensionsLarge = document.getElementById('Order-Images-Dimensions-Large');
+
+    // ProductDimensionsMedium.classList.add('Active');
+
+    ProductSizeSmall.addEventListener('click', () => {
+        ProductSize.className = "";
+        ProductSize.classList.add('Small');
+        // ProductDimensionsSmall.classList.add('Active');
+        // ProductDimensionsMedium.classList.remove('Active');
+        // ProductDimensionsLarge.classList.remove('Active');
+    });
+    ProductSizeMedium.addEventListener('click', () => {
+        ProductSize.className = "";
+        ProductSize.classList.add('Medium');
+        // ProductDimensionsSmall.classList.remove('Active');
+        // ProductDimensionsMedium.classList.add('Active');
+        // ProductDimensionsLarge.classList.remove('Active');
+    });
+    ProductSizeLarge.addEventListener('click', () => {
+        ProductSize.className = "";
+        ProductSize.classList.add('Large');
+        // ProductDimensionsSmall.classList.remove('Active');
+        // ProductDimensionsMedium.classList.remove('Active');
+        // ProductDimensionsLarge.classList.add('Active');
+    });
+
+
 // Order top color
     const ProductTop = document.getElementById('Order-Img-Top');
     const ProductTopBlue = document.getElementById('Order-Top-Color-Blue');
+    const ProductTopPink = document.getElementById('Order-Top-Color-Pink');
+    const ProductTopOrange = document.getElementById('Order-Top-Color-Orange');
+    const ProductTopYellow = document.getElementById('Order-Top-Color-Yellow');
+    const ProductTopWhite = document.getElementById('Order-Top-Color-White');
 
     ProductTopBlue.addEventListener('click', () => {
-        ProductTop.classList.toggle('Top-Blue');
+        ProductTop.className = "";
+        ProductTop.classList.add('Top-Blue');
     });
+    ProductTopPink.addEventListener('click', () => {
+        ProductTop.className = "";
+        ProductTop.classList.add('Top-Pink');
+    });
+    ProductTopOrange.addEventListener('click', () => {
+        ProductTop.className = "";
+        ProductTop.classList.add('Top-Orange');
+    });
+    ProductTopYellow.addEventListener('click', () => {
+        ProductTop.className = "";
+        ProductTop.classList.add('Top-Yellow');
+    });
+    ProductTopWhite.addEventListener('click', () => {
+        ProductTop.className = "";
+        ProductTop.classList.add('Top-White');
+    });
+
+// Order bottom color
+    const ProductBottom = document.getElementById('Order-Img-Bottom');
+    const ProductBottomBlue = document.getElementById('Order-Bottom-Color-Blue');
+    const ProductBottomPink = document.getElementById('Order-Bottom-Color-Pink');
+    const ProductBottomOrange = document.getElementById('Order-Bottom-Color-Orange');
+    const ProductBottomYellow = document.getElementById('Order-Bottom-Color-Yellow');
+    const ProductBottomWhite = document.getElementById('Order-Bottom-Color-White');
+
+    ProductBottomBlue.addEventListener('click', () => {
+        ProductBottom.className = "";
+        ProductBottom.classList.add('Bottom-Blue');
+    });
+    ProductBottomPink.addEventListener('click', () => {
+        ProductBottom.className = "";
+        ProductBottom.classList.add('Bottom-Pink');
+    });
+    ProductBottomOrange.addEventListener('click', () => {
+        ProductBottom.className = "";
+        ProductBottom.classList.add('Bottom-Orange');
+    });
+    ProductBottomYellow.addEventListener('click', () => {
+        ProductBottom.className = "";
+        ProductBottom.classList.add('Bottom-Yellow');
+    });
+    ProductBottomWhite.addEventListener('click', () => {
+        ProductBottom.className = "";
+        ProductBottom.classList.add('Bottom-White');
+    });
+
+    // var colorTopButton = document.getElementsByClassName("Order-Top-Color-Buttons");
+    // var imageTop = document.getElementById('Order-Img-Top');
+    // var i;
+
+    // for (i = 0; i < colorTopButton.length; i++) {
+    //     colorTopButton[i].addEventListener("click", function() {
+    //         imageTop.classList.toggle("Top");
+    //     });
+    // };
     
 
 // Remove Drop-Down menu after clicking a link
